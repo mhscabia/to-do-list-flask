7057f834ef8c046c4ded160620389817c67f74b5
mhscabia <mhscabia@gmail.com>
2023-08-29 14:38:08 -0300

    create db model

--- app.py
+++ app.py
@@ -6,9 +6,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo-db.sqlite'
 
 db = SQLAlchemy(app)
 
+class TodoItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(100))
+    done = db.Column(db.Boolean, default=False)
+
+    def __init__(self, task):
+        self.task = task
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    todo_list = TodoItem.query.all()
+    return render_template('index.html', todo_list=todo_list)
 
 if __name__ == '__main__':
     with app.app_context():
